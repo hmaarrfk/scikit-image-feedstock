c76871f3d3cb6183e0f8dc936dd1537a1855bb12
Mark Harfouche <mark.harfouche@gmail.com>
2018-06-07 11:44:24 -0700

    Actually run tests during build
    Fixes #23
    
    Fail from run_test.py correctly. It must raise an error and not return
    non zero.

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,8 +1,17 @@
 import sys
-import matplotlib
 
-matplotlib.use('Agg')
+try:
+    # in anticipation of matplotlib not being a core dependency
+    import matplotlib
+    matplotlib.use('Agg')
+except ImportError:
+    #  ModuleNotFoundError when 3.5 gets dropped.
+    pass
 
-import skimage
+import pytest
 
-sys.exit(skimage.test(verbose=True))
+failure_code = pytest.main(['--pyargs',  'skimage'])
+
+if failure_code != 0:
+    raise RuntimeError("scikit-image tests failed with exit code {}".format(
+        failure_code))
