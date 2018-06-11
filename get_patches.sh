53a5f5676e5030692829073f6389ffb4aa61bdac
Mark Harfouche <mark.harfouche@gmail.com>
2018-06-11 12:15:28 -0700

    Use versioneer.

--- get_patches.sh
+++ get_patches.sh
@@ -6,7 +6,7 @@ curl -L https://github.com/scikit-image/scikit-image/pull/3103.diff > recipe/pr_
 curl -L https://github.com/scikit-image/scikit-image/pull/3077.diff > recipe/pr_3077_feature_transform_geometry.diff
 curl -L https://github.com/scikit-image/scikit-image/pull/3069.diff > recipe/pr_3069_feature_rectangle_perimeter.diff
 curl -L https://github.com/scikit-image/scikit-image/pull/3057.diff > recipe/pr_3057_feature_bayer2rgb.diff
-
+curl -L https://patch-diff.githubusercontent.com/raw/hmaarrfk/scikit-image/pull/2.diff > recipe/versioneer.diff
 
 # This one breaks the simplification of the build process
 curl -L https://github.com/scikit-image/scikit-image/pull/3129.diff > recipe/pr_3129_remove_matplotlib_dependency.diff
